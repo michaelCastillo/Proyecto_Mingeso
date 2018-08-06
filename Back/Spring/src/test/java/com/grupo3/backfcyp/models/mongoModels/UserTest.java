6c92a599241e345172670aeb988f3a06f52d6c2c
MichaelCastillo <michael.castillo.p96@gmail.com>
2018-08-05 23:27:29 -0400

    se agrega el test del controlador del usuario

--- Back/Spring/src/test/java/com/grupo3/backfcyp/models/mongoModels/UserTest.java
+++ Back/Spring/src/test/java/com/grupo3/backfcyp/models/mongoModels/UserTest.java
@@ -1,19 +1,24 @@
 package com.grupo3.backfcyp.models.mongoModels;
 
 import com.grupo3.backfcyp.models.User;
-import com.grupo3.backfcyp.repositories.UserRepository;
+import com.grupo3.backfcyp.repositories.*;
 import com.grupo3.backfcyp.services.UserService;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import sun.rmi.server.UnicastServerRef;
@@ -25,8 +30,10 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 
+@SpringBootTest
+@ContextConfiguration
 public class UserTest {
 
     private String name = "prueba 1";
@@ -39,14 +46,30 @@ public class UserTest {
 
     private User userExit;
 
-    @MockBean
-    public UserRepository userRepository;
-    @MockBean
+    @Autowired
     public UserService userService;
 
+    @MockBean
+    public RoleRepository roleRepository;
+
+    @MockBean
+    public UserRepository userRepository;
+
+    @MockBean
+    public CoordinationRepository coordinationRepository;
+
+    @MockBean
+    public CareerRepository careerRepository;
+
+    @MockBean
+    private ClassRepository classRepository;
+
+    private User userReal;
 
     @Before
     public void prepare(){
+        userReal = userRepository.findUserById(new Long(11));
+
         userTest = new User();
         userTest.setName(name);
         userTest.setPassword(password);
@@ -71,11 +94,39 @@ public class UserTest {
     }
 
     @Test
-    public void testController(){
-        assertThat(userService.getUsers().equals(userRepository.findAll()));
+    public void testGets(){
+        //userService.getUserById(new Long(11)).getProblems();
+        /*userReal.getClasses_students();
+        userReal.getCareers();
+        userReal.getClasses_teachers();
+        userReal.getCoordCoordinations();
+        userReal.getProblems();
+        userReal.getSolutions();*/
     }
 
-    
+    @Test
+    public void testController(){
+
+        System.out.println(userService.getUsers());
+        assertThat(userService.getUsers().equals(userRepository.findAll()));
+        userService.getUserById(new Long(1));
+
+
+    }
+
+    @Configuration
+    static class Config {
+
+        @Bean
+        public UserService userService() {
+            // Assuming MessagingProperties has default ctor.
+            return new UserService();
+        }
+
+        // Same for MessagingPropertiesRefactor
+    }
+
+
 
 
 
