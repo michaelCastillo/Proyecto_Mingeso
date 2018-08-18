cf60266a30fcecbab354f5bb96f021498cf5b6ab
MichaelCastillo <michael.castillo.p96@gmail.com>
2018-08-18 11:48:03 -0300

    Se añade Singleton al utilizar la api de la ejecución del código.

--- Back/Spring/src/main/java/com/grupo3/backfcyp/strategy/StrategyJava.java
+++ Back/Spring/src/main/java/com/grupo3/backfcyp/strategy/StrategyJava.java
@@ -20,78 +20,10 @@ public class StrategyJava implements Strategy {
 
     @Override
     public List<Results> executeProgram(Test test, ArrayList<String> o_inputs, CodeRepository codeRepository) {
-        List<Results> results = new ArrayList<>();
-        String clientId = "9cf4866b-8e10-4e3a-84bf-2ee26e40c992"; //Replace with your client ID
-        ArrayList<String> outputs = new ArrayList<String>();
-
-        String output = new String();
-        for(int i = 0; i<o_inputs.size(); i++) {
-            try {
-                URL url = new URL("https://run.glot.io/languages/java/latest");
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setDoOutput(true);
-                connection.setRequestMethod("POST");
-                connection.setRequestProperty("Content-Type", "application/json");
-                connection.setRequestProperty("authorization", "Token f2b9a615-ef8b-4238-b93b-f27eb13a1d6f");
-
-                String stdin;
-                if (!o_inputs.isEmpty()) {
-                    stdin = "\"stdin\":\"" + o_inputs.get(i) + "\"";
-                } else {
-                    stdin = "\"stdin\":\"\"";
-                }
-
-                String input = "{" + stdin + ",\"files\": [{\"name\":\"Main.java\", \"content\":  \"" + test.getCode(codeRepository) + "\"}]}";
-                System.out.println(input);
-
-                OutputStream outputStream = connection.getOutputStream();
-                outputStream.write(input.getBytes());
-                outputStream.flush();
-
-                if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-
-                    throw new RuntimeException("Please check your inputs : HTTP error code : " + connection.getResponseCode());
-                }
-
-                BufferedReader bufferedReader;
-                bufferedReader = new BufferedReader(new InputStreamReader(
-                        (connection.getInputStream())));
-
-
-                System.out.println("Output from JDoodle .... \n");
-                while ((output = bufferedReader.readLine()) != null) {
-                    results.add(parseResults(output));
-                    outputs.add(output);
-                    System.out.println(output);
-                }
-
-                connection.disconnect();
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-                return null;
-            } catch (IOException e) {
-                e.printStackTrace();
-                return null;
-            }
-
-        }
-        return results;
+        DoodleConnection doodleConnection = DoodleConnection.getInstance();
+        doodleConnection.setLanguaje("java");
+        return doodleConnection.executeProgram(test,o_inputs,codeRepository);
     }
-    private Results parseResults(String resultsOnJson){
-        ObjectMapper mapper = new ObjectMapper();
-        Results results = new Results();
-        try {
-            results = mapper.readValue(resultsOnJson, Results.class);
 
-        } catch (JsonParseException e) {
-            e.printStackTrace();
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return results;
-    }
 
 }
