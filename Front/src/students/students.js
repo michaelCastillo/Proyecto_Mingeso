47afe430c6ca0ecf693e3aa9dec6905ef94df65d
Javier Frez <javier.frez@usach.cl>
2018-08-26 03:06:23 -0300

    Ordenamiento de codigo y Heuristicas.
    Heuristica:
    Pantalla de carga.

--- Front/src/students/students.js
+++ Front/src/students/students.js
@@ -1,21 +1,25 @@
 import React, {Component} from 'react';
 import {ListGroup,ListGroupItem,Grid,Row,Col,Label,Button} from 'react-bootstrap';
 import axios from 'axios';
+import ReactLoading from "react-loading";
+import './students.css'
 
 
-class Students extends Component{
+class Students extends Component
+{
 
-  constructor(props){
-    super(props);
+    constructor(props)
+    {
+        super(props);
 
-    this.state= {
-        role:"student", 
-        students:[]
+        this.state= {
+            role:"student", 
+            students:[],
+            ready:false,
+        }
     }
 
-  }
-
-      componentDidMount  = () => {
+    componentDidMount  = () => {
         let user = {
           role:"student"
         };
@@ -23,48 +27,63 @@ class Students extends Component{
             .then(res => {
                 const students=res.data;
                 this.setState({ students });
+                this.setState({ready:true});
             }).catch(error => {
                 console.log(error.response)
             });
-      };
-render(){
-    return(
-      <div id="students" className = " students"  >
+    };
+
+    handleAlumnos()
+    {
+        if(!this.state.ready)
+        {
+            return(
+                <section id="carga">
+                    <ReactLoading type={"spin"} color={"#000"} height={667} width={375}/>
+                </section>
+            );
+        }
+        else
+        {
+            return(
+                this.state.students.map((student) => {
+                    return (
+                  <Row className="grid-show">
+                      <Col sm ={6} md={12}>
+                        <ListGroup>
+                              
+                        <ListGroupItem style={{background:'#66B3DD'}} href={`/users/${student.id}`}> {student.name}</ListGroupItem>
+                        </ListGroup>
+                      </Col>
+                  </Row>
+
+                 );})
+            );
+        }
+    }
+
+
+    render()
+    {
+        return(
+            <div id="students" className = " students"  >
                 <br/>
                 <br/>
                 <br/>
                 <Grid>
                     <Row > 
                         <Col md={12} xs={12}>
-                        <h1  className="center"><Label id="title">Lista Alumnos:</Label></h1>
+                            <h1  className="center"><Label id="title">Lista Alumnos:</Label></h1>
                         </Col>
                     </Row>
                     <br/>
                     <br/>
                     <br/>
-                    { 
-                      this.state.students.map((student) => {
-                        return (
-                      <Row className="grid-show">
-                          <Col sm ={6} md={12}>  
-                            <ListGroup>
-                                  
-                            <ListGroupItem style={{background:'#66B3DD'}} href={`/users/${student.id}`}> {student.name}</ListGroupItem>
-                            </ListGroup>
-                          </Col>
-                      </Row>
-
-                     );})
-                  }
+                    {this.handleAlumnos()}
                 </Grid>  
-        </div>                  
-
-                  
-    );
-
-
-
-}
+            </div>          
+        );
+    }
 
 }
 
